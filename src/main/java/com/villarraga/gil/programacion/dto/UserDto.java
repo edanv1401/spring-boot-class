1bda41b681f72bbd48aac2879383683bf58475c2
luisda512 <ldgilm@itc.edu.co>
2024-09-11 17:08:51 -0500

    le agregue encryptado

--- src/main/java/com/villarraga/gil/programacion/dto/UserDto.java
+++ src/main/java/com/villarraga/gil/programacion/dto/UserDto.java
@@ -13,4 +13,20 @@ public class UserDto {
     private String username;
     private String password;
     private Role role;
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
 }
--- src/main/java/com/villarraga/gil/programacion/services/user/UserServiceImpl.java
+++ src/main/java/com/villarraga/gil/programacion/services/user/UserServiceImpl.java
@@ -1,6 +1,8 @@
 package com.villarraga.gil.programacion.services.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -14,16 +16,22 @@ public class UserServiceImpl implements IUserService{
     
     public UserServiceImpl(@Autowired IUserRepository repository) {
         this.repository = repository;
+        this.passwordEncoder = new BCryptPasswordEncoder();
         mapper = new ObjectMapper();
     }
+    private final PasswordEncoder passwordEncoder;
 
     @Override
     public UserDto singIn(UserDto user) {
+        String encoderPassw = this.passwordEncoder.encode(user.getPassword());
+        user.setPassword(encoderPassw);
         return mapper.convertValue(
             repository
             .findByUsernameAndPassword(user.getUsername(), user.getPassword())
             .orElse(null), 
         UserDto.class);
+
+
     }
     
 }
